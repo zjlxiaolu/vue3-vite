c1660499231bb01b7b0f05666f01db56e7345612
zjlxiaolu <zjlxiaolu@163.com>
2023-06-16 16:37:19 +0800

    build docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/vue3-vite/',
+    base: 'vue3-vite',
     build: {
         outDir: 'docs'
     }
